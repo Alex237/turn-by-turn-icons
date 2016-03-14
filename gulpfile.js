f7c10a7b03633fdc524fe607317e42cccba73bef
Lou Huang <lou@louhuang.com>
2016-03-14 13:07:32 -0400

    Add build scripts

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var fs = require('fs')
+var gulp = require('gulp')
+var svgSprite = require('gulp-svg-sprite')
+
+var svgConfig = {
+  log: 'verbose',
+  shape: {
+    id: {
+      generator: 'icon-%s'
+    }
+  },
+  mode: {
+    symbol: {
+      dest: '.',
+      sprite: 'icons',
+      inline: true
+    }
+  }
+}
+
+gulp.task('default', function () {
+  return gulp.src('assets/*.svg')
+    .pipe(svgSprite(svgConfig))
+    .pipe(gulp.dest('./dist'))
+})
